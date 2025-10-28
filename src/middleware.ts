fe2b66d128d58d0b26d5349dd8f2b43d2ac2cafa
Shinia-Gupta <shiniavit@gmail.com>
2025-10-28 15:33:11 +0530

    role based access control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 
 // const isProtectedRoute=createRouteMatcher('/user-profile');
@@ -7,11 +8,18 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
+//route matcher for admin route
+const isAdminRoute=createRouteMatcher(["/admin(.*)"])
 
 export default clerkMiddleware(async (auth,req)=>{
   // if(isProtectedRoute(req)) await auth.protect()  //protect method redirects the user page to sign in page directly if not signed in
 
 const {userId,redirectToSignIn}=await auth();
+
+if(isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role!=="admin"){
+  const url=new URL("/",req.url)
+  return NextResponse.redirect(url)
+}
 if(!userId && !isPublicRoute(req)){
   return redirectToSignIn();
 }
